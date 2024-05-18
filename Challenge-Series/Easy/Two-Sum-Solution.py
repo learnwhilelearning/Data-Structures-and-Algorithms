2b2e69f01f56778b73da290a774e12f7e1a85101
learnwhilelearning <109062125+learnwhilelearning@users.noreply.github.com>
2024-05-18 16:10:51 +0530

    Create Two-Sum-Solution.py

---
+++ Challenge-Series/Easy/Two-Sum-Solution.py
@@ -0,0 +1,24 @@
+#Solution 1
+class Solution:
+    def twoSum(self, nums ,target):
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+
+
+
+
+#Solution 2
+class Solution:
+    def twoSum(self, nums ,target):
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return []
